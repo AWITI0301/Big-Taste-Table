f317f60ea3e722fef29928821a360bd8064c5981
AWITI0301 <johnoketchawiti@gmail.com>
2022-06-22 15:35:19 +0300

    add gallery section in html

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// // const options = {
+// // 	method: 'GET',
+// // 	headers: {
+// // 		'X-RapidAPI-Key': '860babcf51msh95cb7aad9c79e8cp177742jsn83dc651616e5',
+// // 		'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+// // 	}
+// // };
+
+// // fetch('https://yummly2.p.rapidapi.com/feeds/auto-complete?q=chicken%20soup', options)
+// // 	.then(response => response.json())
+// // 	.then(response => console.log(response))
+// // 	.catch(err => console.error(err));
+
+
+//     function fetchFood(){
+//         fetch('https://yummly2.p.rapidapi.com/feeds/auto-complete?q=chicken%20soup')
+//         .then(response => response.json())
+//         .then(food =>{
+//             console.log(food)
+//         })
+//     }
+//     fetchFood()
