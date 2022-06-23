18ea0a1e8dba01f5fa0e0be3101fe87f6e763b9c
AWITI0301 <johnoketchawiti@gmail.com>
2022-06-24 01:20:33 +0300

    style search seaction of the ingredients

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,56 @@
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '860babcf51msh95cb7aad9c79e8cp177742jsn83dc651616e5',
-// 		'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
-// 	}
-// };
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '860babcf51msh95cb7aad9c79e8cp177742jsn83dc651616e5',
+		'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
+	}
+};
 
-// fetch('https://yummly2.p.rapidapi.com/feeds/auto-complete?q=chicken%20soup', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-
-// //     function fetchFood(){
-// //         fetch('https://yummly2.p.rapidapi.com/feeds/auto-complete?q=chicken%20soup')
-// //         .then(response => response.json())
-// //         .then(food =>{
-// //             console.log(food)
-// //         })
-// //     }
-// //     fetchFood()
+fetch('https://yummly2.p.rapidapi.com/feeds/auto-complete?q=chicken%20soup', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
 
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
+//     function fetchFood(){
+//         fetch('https://yummly2.p.rapidapi.com/feeds/auto-complete?q=chicken%20soup')
+//         .then(response => response.json())
+//         .then(food =>{
+//             console.log(food)
+//         })
+//     }
+//     fetchFood()
 
-menu.onclick = () =>{
 
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
+// let menu = document.querySelector('#menu-bar');
+// let navbar = document.querySelector('.navbar');
 
-}
+// menu.onclick = () =>{
 
-window.onscroll = () =>{
+//   menu.classList.toggle('fa-times');
+//   navbar.classList.toggle('active');
 
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
+// }
 
-  if(window.scrollY > 60){
-    document.querySelector('#scroll-top').classList.add('active');
-  }else{
-    document.querySelector('#scroll-top').classList.remove('active');
-  }
+// window.onscroll = () =>{
 
-}
+//   menu.classList.remove('fa-times');
+//   navbar.classList.remove('active');
 
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
-}
+//   if(window.scrollY > 60){
+//     document.querySelector('#scroll-top').classList.add('active');
+//   }else{
+//     document.querySelector('#scroll-top').classList.remove('active');
+//   }
 
-function fadeOut(){
-  setInterval(loader, 3000);
-}
+// }
 
-window.onload = fadeOut();
+// function loader(){
+//   document.querySelector('.loader-container').classList.add('fade-out');
+// }
+
+// function fadeOut(){
+//   setInterval(loader, 3000);
+// }
+
+// window.onload = fadeOut();
